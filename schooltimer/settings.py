c361d5d3447c9ae8f3b9621caa6c60103ae8f983
amnah1438 <__@domain.com>
2025-11-27 23:27:46 +0300

    تعديل عل الاعدات

--- schooltimer/settings.py
+++ schooltimer/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # ==========================
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # عند التحزيم (collectstatic)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
